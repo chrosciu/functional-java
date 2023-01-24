5c936b59dcb1c803cd3ab87bdf951b7ff7cb9048
Marcin Chrost <mchrost82@gmail.com>
2023-01-24 17:26:05 +0100

    J09b_SpliteratorTest

--- src/main/java/info/solidsoft/java8/people/PersonDao.java
+++ src/main/java/info/solidsoft/java8/people/PersonDao.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -19,6 +20,10 @@ public class PersonDao {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public Iterator<Person> getPeopleIterator() {
+        return loadPeopleDatabase().iterator();
     }
 
     private BufferedReader open(String fileName) {
