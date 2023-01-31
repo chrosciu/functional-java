6d63e6529208ff72607ecb37b3a26e9b69fc88ce
Marcin Chrost <mchrost82@gmail.com>
2023-01-31 15:09:13 +0100

    Reducing all rules to one

--- src/main/java/info/solidsoft/java8/scoring/DynamicCalculator.java
+++ src/main/java/info/solidsoft/java8/scoring/DynamicCalculator.java
@@ -5,10 +5,7 @@ import java.util.List;
 public class DynamicCalculator {
 
     public Scoring calculate(Person person, LoanApplication application, List<Rule> rules) {
-        Scoring scoring = new Scoring(100);
-        for (Rule rule : rules) {
-            scoring = rule.apply(scoring, person, application);
-        }
-        return scoring;
+        Rule allRules = rules.stream().reduce(Rule.IDENTITY, Rule::andThen);
+        return allRules.apply(new Scoring(100), person, application);
     }
 }
--- src/main/java/info/solidsoft/java8/scoring/Rule.java
+++ src/main/java/info/solidsoft/java8/scoring/Rule.java
@@ -3,4 +3,10 @@ package info.solidsoft.java8.scoring;
 public interface Rule {
 
     Scoring apply(Scoring scoring, Person person, LoanApplication loanApplication);
+
+    default Rule andThen(Rule after) {
+        return (scoring, person, loanApplication) -> after.apply(this.apply(scoring, person, loanApplication), person, loanApplication);
+    }
+
+    static Rule IDENTITY = (scoring, person, loanApplication) -> scoring;
 }
